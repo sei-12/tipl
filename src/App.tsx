c76834dff3160e5662e9c325c1e85a66a6430e0c
おもちくん <sei.t11@icloud.com>
2023-03-22 16:05:52 +0900

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { LinkList, LinkListProps } from './link-items';
 import { Link } from './models/link';
 import { Tag } from './models/tag';
 import { fetch_parsed_link_data, fetch_parsed_tag_datas } from './test/decoy-data';
 
 function App() {
 	const [tags,set_tags] = useState<Tag[]>([])
-	const [show_links,set_show_links] = useState<Link[]>([])
+	const [filted_links, set_filted_links] = useState<Link[]>([])
 	const [links , set_links ] = useState<Link[]>([])
-	const [focus_link,set_focus_link] = useState<Link | null> (null)
+	const [focus_link_id,set_focus_link_id] = useState<number | null> (null)
+
+	const link_list_props : LinkListProps = {
+		focus_link_id:focus_link_id,
+		set_focus_link_id:set_focus_link_id,
+		list_up_links:links
+	}
 
 	useEffect(() => {
 		(async () => {
@@ -23,9 +30,13 @@ function App() {
 		})()
 	},[])
 
+	useEffect(() => {
+		set_focus_link_id(null)
+	},[filted_links])
+
 	return (
 		<div className="App">
-
+			<LinkList {...link_list_props} />
 		</div>
 	);
 }
