5c83e38ddbff6be69d1e14921ba234488a44e683
おもちくん <sei.t11@icloud.com>
2023-03-22 19:52:39 +0900

    remove search word input box

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { LinkList, LinkListProps } from './link-items';
 import { Link } from './models/link';
 import { Tag } from './models/tag';
-import { SearchWordInputBox, SearchWordInputBoxProps } from './search-word-input-box';
 import { fetch_parsed_link_data, fetch_parsed_tag_datas } from './test/decoy-data';
 
 function App() {
@@ -16,12 +15,6 @@ function App() {
 		focus_link_id:focus_link_id,
 		set_focus_link_id:set_focus_link_id,
 		list_up_links:filted_links
-	}
-
-	const search_word_input_box_props : SearchWordInputBoxProps = {
-		links:links,
-		filted_links:filted_links,
-		set_filted_links:set_filted_links
 	}
 
 	useEffect(() => {
@@ -43,7 +36,6 @@ function App() {
 
 	return (
 		<div className="App">
-			<SearchWordInputBox {...search_word_input_box_props}/>
 			<LinkList {...link_list_props} />
 		</div>
 	);
