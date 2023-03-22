87610a104fe0e487cbe64420dadcf20937d5d7cf
おもちくん <sei.t11@icloud.com>
2023-03-22 19:59:02 +0900

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { LinkFilter, LinkFilterProps } from './link-filter';
 import { LinkList, LinkListProps } from './link-items';
 import { Link } from './models/link';
 import { Tag } from './models/tag';
@@ -15,6 +16,12 @@ function App() {
 		focus_link_id:focus_link_id,
 		set_focus_link_id:set_focus_link_id,
 		list_up_links:filted_links
+	}
+
+	const link_filter_props : LinkFilterProps = {
+		tags:tags,
+		links:links,
+		set_filted_links:set_filted_links
 	}
 
 	useEffect(() => {
@@ -36,6 +43,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<LinkFilter {...link_filter_props}/>
 			<LinkList {...link_list_props} />
 		</div>
 	);
