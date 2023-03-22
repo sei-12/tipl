d8c42b38555b2aa800fe05ce25591e8d090f2358
おもちくん <sei.t11@icloud.com>
2023-03-22 19:18:46 +0900

    fix type

--- src/tag-selector.tsx
+++ src/tag-selector.tsx
@@ -5,7 +5,7 @@ export type TagSelectoorProps = {
     is_show:boolean
     tags:Tag[]
     exclude_ids:number[]
-    set_result_id_buf:Dispatch<SetStateAction<number>>
+    set_result_id_buf:Dispatch<SetStateAction<number | null>>
 }
 
 export const TagSelectoor = (p:TagSelectoorProps) => {
