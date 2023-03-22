702d70b840b49c5d7e22582a65eb7a73b5cc7413
おもちくん <sei.t11@icloud.com>
2023-03-22 19:14:10 +0900

    add tag selector definition

---
+++ src/tag-selector.tsx
@@ -0,0 +1,15 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import { Tag } from './models/tag'
+
+export type TagSelectoorProps = {
+    is_show:boolean
+    tags:Tag[]
+    exclude_ids:number[]
+    set_result_id_buf:Dispatch<SetStateAction<number>>
+}
+
+export const TagSelectoor = (p:TagSelectoorProps) => {
+    return (
+        <div></div>
+    )
+}
