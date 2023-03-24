68895bc7e47b154561100f3d0ac7afb610c062c6
おもちくん <sei.t11@icloud.com>
2023-03-24 12:14:15 +0900

    add electron

---
+++ electron/src/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron'
+import path = require('path')
+
+
+const mainURL = `file:${__dirname}/../../index.html`
+
+const createWidnow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 500,
+        height: 500,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    })
+    mainWindow.loadURL(mainURL)
+}
+
+app.whenReady().then(() => {
+    createWidnow()
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWidnow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
