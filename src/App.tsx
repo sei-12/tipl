003a7641db98b33c9614301079a73238ad2afaca
おもちくん <sei.t11@icloud.com>
2023-03-25 22:22:39 +0900

    move file create new tag

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { useHotkeys } from 'react-hotkeys-hook'
 import { Dispatch , SetStateAction} from 'react'
 import { Prompt, PromptProps } from './prompt';
 import { HotkeyScapes, Hotkey_Scape } from './hotkeys';
+import { CreateNewTag,CreateNewTagProps } from './create-new-tag';
 
 const next_id = function(links_tags:Link[] | Tag[]) : number{
 	return links_tags[links_tags.length - 1].ID + 1
@@ -73,58 +74,6 @@ function Hotkey(p:HotkeyProps){
 
 	return (
 		<div></div>
-	)
-}
-
-type CreateNewTagProps = {
-	tags:Tag[]
-	set_tags:Dispatch<SetStateAction<Tag[]>>
-}
-
-const CreateNewTag = (p:CreateNewTagProps) => {
-
-	const create_new_tag = () => {
-		set_prompt_is_show(true)
-	}
-
-	const [prompt_is_show,set_prompt_is_show] = useState<boolean>(false)
-	const [prompt_result_buf,set_prompt_result_buf] = useState<string|null>(null)
-	const prompt_props : PromptProps = {
-		is_show:prompt_is_show,
-		set_is_show:set_prompt_is_show,
-		message:"tag name",
-		set_result_buf:set_prompt_result_buf
-	}
-
-	useEffect(() => {
-		document.addEventListener("keydown",(e) => {
-			if([HotkeyScapes.Normal].includes(Hotkey_Scape.get()) == false) return
-			if(
-				e.metaKey && 
-				e.shiftKey && 
-				e.altKey == false && 
-				e.ctrlKey == false && 
-				e.key == "n"){
-					create_new_tag()
-			}
-		})	
-	},[])
-
-	useEffect(() => {
-		if(prompt_result_buf == null || prompt_result_buf == "") return
-		let id = next_id(p.tags)
-		let new_tag : Tag = {
-			ID:id,
-			title:prompt_result_buf
-		}
-		p.set_tags([...p.tags,new_tag])
-	},[prompt_result_buf])
-
-	return (
-		<div>
-			<input type="button" value="create new tag" onClick={create_new_tag}/>
-			<Prompt {...prompt_props}/>
-		</div>
 	)
 }
 
