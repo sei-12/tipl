7bc2c88c8b67a5cac78e05bd1de048e770f6d6ce
おもちくん <sei.t11@icloud.com>
2023-03-27 16:30:26 +0900

    add auto scroll

--- src/link-list.tsx
+++ src/link-list.tsx
@@ -1,12 +1,14 @@
 import React, { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { Link } from "./models/link";
 import './link-list.css'
+import { useState } from "react";
 
 type LinkItemProps = {
     key:number
     focus_link_id:number | null
     set_focus_link_id:Dispatch<SetStateAction<number | null>>
     data:Link
+    set_focus_item_offset_top:Dispatch<SetStateAction<number | null>>
 } 
 
 
@@ -18,6 +20,11 @@ const LinkItem = function(p:LinkItemProps){
         p.set_focus_link_id(p.data.ID)
     }
 
+    const get_item_top_y = function():number{
+        if( div_ref.current == null ) return 0
+        return div_ref.current.offsetTop
+    }
+
     useEffect(() => {
         if(p.focus_link_id == null){
             div_ref.current!.classList.remove('focus-item')
@@ -26,6 +33,7 @@ const LinkItem = function(p:LinkItemProps){
 
         if(p.focus_link_id == p.data.ID){
             div_ref.current!.classList.add('focus-item')
+            p.set_focus_item_offset_top(get_item_top_y())
         }else{
             div_ref.current!.classList.remove('focus-item')
         }
@@ -50,6 +58,7 @@ export const LinkList = function( p : LinkListProps ){
     const map_links = function(){
         return p.list_up_links.map( link => {
             let props : LinkItemProps = {
+                set_focus_item_offset_top:set_focus_item_offset_top,
                 key:link.ID,
                 focus_link_id:p.focus_link_id,
                 set_focus_link_id:p.set_focus_link_id,
@@ -62,8 +71,23 @@ export const LinkList = function( p : LinkListProps ){
         })
     }
 
+    const list_div = useRef<HTMLDivElement>(null)
+    const [focus_item_offset_top,set_focus_item_offset_top] = useState<number | null>(null)
+    // フォーカスしているリンクが表示されるように自動でスクロールする
+
+    useEffect(() => {
+        if( list_div.current == null ) return
+        if( focus_item_offset_top == null ) return
+        list_div.current.scrollTop = focus_item_offset_top - list_div.current.offsetTop
+    },[focus_item_offset_top])
+    
+    useEffect(() => {
+        if(p.focus_link_id == null){
+            set_focus_item_offset_top(null)
+        }
+    },[p.focus_link_id])
     return (
-        <div>
+        <div className="list-div" ref={list_div} >
             {map_links()}
         </div>
     )
